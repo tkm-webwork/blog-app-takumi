9e27d1f1fb187c19d3a4966890db3ab32e75d421
takumi osanai <84653168+tkm-webwork@users.noreply.github.com>
2021-08-22 16:15:07 +0900

    基本的な表示を作成(index,about)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # URLを作る場所
+  root to: 'home#index'
+  get '/about' => 'home#about'
 end
