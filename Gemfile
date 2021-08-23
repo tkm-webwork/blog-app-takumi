0ce527e2b052a5b748bdec3a8213b8e9598c84e3
takumi osanai <84653168+tkm-webwork@users.noreply.github.com>
2021-08-23 12:31:27 +0900

    activeのよるログイン機能実装

--- Gemfile
+++ Gemfile
@@ -24,8 +24,10 @@ gem 'jbuilder', '~> 2.7'
 gem 'annotate'
 gem 'better_errors'
 gem 'binding_of_caller'
+gem 'devise'
 gem 'faker'
 gem 'hamlit'
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,9 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    
+
     @articles = Article.all
   end
 
